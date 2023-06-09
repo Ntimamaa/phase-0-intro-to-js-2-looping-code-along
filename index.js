cc480d9bfd0c36002ce5f8dd239c6937c843f13d
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-06-09 16:23:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+function writeCards(names) {
+  const messages = [
+    // `Thank you, ${Aki}, for the wonderful surprise gift!`,
+    // `Thank you, ${Ollie}, for the wonderful surprise gift!`,
+    // `Thank you, ${Guadalupe}, for the wonderful surprise gift!`
+  ];
+  let name;
+  for (name of names) {
+     messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+  }
+  return messages;
+}
+console.log(writeCards("Guadalupe"))
+console.log(writeCards("Aki"))
+console.log(writeCards("Ollie"))
+
+
+
+function countDown(number) {
+  const countdownArray = [];//empty array
+  for (let i = number; i >= 0; i--) {
+    countdownArray.push(i);
+  }
+  for (let i = 0; i < countdownArray.length; i++) {
+    console.log(countdownArray[i]);
+  }
+
+}
+
